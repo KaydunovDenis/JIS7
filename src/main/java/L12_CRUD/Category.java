8d1296051432379238910b2a98e604b863817c3b
KaydunovDenis <deniskaydunov@gmail.com>
2021-03-17 16:41:39 +0300

    Add Enum Category

--- src/main/java/L12_CRUD/Category.java
+++ src/main/java/L12_CRUD/Category.java
@@ -1,4 +1,25 @@
 package L12_CRUD;
 
 public enum Category {
+    PHONE ("Телефон"),
+    TABLET("Планшет"),
+    NOTEBOOK("Ноутбук"),
+    COMPUTER("Компьютер");
+
+    private String title;
+
+    Category(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "title='" + title + '\'' +
+                '}';
+    }
 }
