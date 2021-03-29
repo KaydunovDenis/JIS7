729980fbbdf2ba31f76d4bd13c9bc739b4a179af
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2021-03-29 20:39:33 +0300

    Update LightColorDetectorTest.java
    //**********@Checked***************//

--- src/main/java/L4/task3_ColorDetector/LightColorDetectorTest.java
+++ src/main/java/L4/task3_ColorDetector/LightColorDetectorTest.java
@@ -1,5 +1,5 @@
 package L4.task3_ColorDetector;
-
+//**********@Checked***************//
 public class LightColorDetectorTest {
     public static void main(String[] args) {
         System.out.printf("Task completed.");
