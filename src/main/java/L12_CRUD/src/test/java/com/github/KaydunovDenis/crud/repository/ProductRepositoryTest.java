333ab4e379335dbbde2bfb450ae1a8b9a37d7a6c
Denis Kaydunov <deniskaydunov@gmail.com>
2021-04-12 08:47:06 +0300

    I write several tests.

---
+++ src/main/java/L12_CRUD/src/test/java/com/github/KaydunovDenis/crud/repository/ProductRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.github.KaydunovDenis.crud.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductRepositoryTest {
+
+    @Test
+    void initialProductRepository() {
+        ProductRepository productRepository = new ProductRepository();
+        Assertions.assertTrue(productRepository.getPRODUCTS().isEmpty());
+
+        productRepository.initialProductRepository();
+        Assertions.assertFalse(productRepository.getPRODUCTS().isEmpty());
+    }
+}
