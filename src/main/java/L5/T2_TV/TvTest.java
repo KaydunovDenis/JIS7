ae3f089459cc3cddb57867ca8e62bc55a5e60a89
Denis Kaydunov <deniskaydunov@gmail.com>
2021-04-15 00:45:55 +0300

    L5T2_Tv has done.

---
+++ src/main/java/L5/T2_TV/TvTest.java
@@ -0,0 +1,19 @@
+package L5.T2_TV;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TvTest {
+    Tv tv = new Tv(0,0, "Horizont", false);
+
+    @Test
+    void toSwithBack() {
+        Assert.assertEquals(0, tv.getCurrentChanel());
+        tv.toSwithBack();
+        Assert.assertNotEquals(-1, tv.getCurrentChanel());
+
+        tv.turnOn();
+        tv.toSwithBack();
+        Assert.assertEquals(-1, tv.getCurrentChanel());
+    }
+}
