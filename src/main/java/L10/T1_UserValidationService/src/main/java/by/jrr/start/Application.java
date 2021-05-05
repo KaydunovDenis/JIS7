9c1871632353a6f783e3d8c419d16fa2e63dea30
Denis Kaydunov <deniskaydunov@gmail.com>
2021-05-05 20:07:22 +0300

    Add some files

---
+++ src/main/java/L10/T1_UserValidationService/src/main/java/by/jrr/start/Application.java
@@ -0,0 +1,8 @@
+package L10.T1_UserValidationService.src.main.java.by.jrr.start;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
