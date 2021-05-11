2a36a2d225cca5c806f804e21db91a7c003c6efb
Denis Kaydunov <deniskaydunov@gmail.com>
2021-05-11 19:47:08 +0300

    Add files from lecture

---
+++ src/main/java/Lecture/NestedClass/OuterClass.java
@@ -0,0 +1,14 @@
+package Lecture.NestedClass;
+
+public class OuterClass {
+    String outerName;
+
+    public class InnerClass {
+        private String innerName;
+    }
+
+    public static class NestedClass {
+
+    }
+
+}
